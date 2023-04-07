f014cff349a462f100ca78214cc26f40751b1687
nurbol0tt <fairtail764@gmail.com>
2023-04-07 22:46:32 +0600

    add docs

--- DreamTeam/urls.py
+++ DreamTeam/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('rest_framework.urls')),
-    path('api/', include('apps.users.urls')),
-    path('api/', include('apps.resume.urls.urls_main')),
-]
+    path('user/', include('apps.users.urls')),
+    path('resume/', include('apps.resume.urls.urls_main')),
+
+] + doc_urls
